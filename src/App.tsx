d11d67d70acb02b47c365f11e338fa7757ee9854
Fendo181 <fendo181git@gmail.com>
2023-11-09 21:11:46 +0900

    feat: 指定してキーワードをランダムに出力するようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const texts = [
+    'じゃがいも',
+    '楽天西友ネットスーパー',
+    'ハイボール',
+  ];
+
+  // テキストをランダムに表示する
+  const [randomText, setRandomText] = useState('');
+
+  // ボタンがクリックされたときのハンドラー
+  const generateRandomText = () => {
+    const randomIndex = Math.floor(Math.random() * texts.length);
+    setRandomText(texts[randomIndex]);
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <h3>Next Endu Hint Generate</h3>
+        <p>{randomText}</p>
+        <button onClick={generateRandomText}>Next Endu Hint!</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>こんにちは！！！</p>
         <a
           className="App-link"
           href="https://reactjs.org"
